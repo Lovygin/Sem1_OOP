6f3d943d31264a3ef52096e5283a7b8236819823
Aleksandr Lovygin <Shurik.lovygin@gmail.com>
2024-02-11 11:51:39 +0300

    first try OOP

---
+++ src/services/VendingMachine.java
@@ -0,0 +1,64 @@
+package services;
+
+import domen.Product;
+
+import java.util.List;
+
+public class VendingMachine {
+    private Holder holder;
+    private Display display;
+    private  CoinDespenser coinDespenser;
+    private  List<Product> products;
+
+    public VendingMachine(Holder holder, Display display, CoinDespenser coinDespenser, List<Product> products) {
+        this.holder = holder;
+        this.display = display;
+        this.coinDespenser = coinDespenser;
+        this.products = products;
+    }
+
+    public VendingMachine() {
+    }
+
+    public Holder getHolder() {
+        return holder;
+    }
+
+    public void setHolder(Holder holder) {
+        this.holder = holder;
+    }
+
+    public Display getDisplay() {
+        return display;
+    }
+
+    public void setDisplay(Display display) {
+        this.display = display;
+    }
+
+    public CoinDespenser getCoinDespenser() {
+        return coinDespenser;
+    }
+
+    public void setCoinDespenser(CoinDespenser coinDespenser) {
+        this.coinDespenser = coinDespenser;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    /**
+     * @apiNote добавление продукта в аппарат
+     * @param product объект добавляемого продукта
+     */
+    public void addProduct(Product product) {
+        if (product.getPrice() <= 0) {
+            product.setPrice(100);
+        }
+    }
+}
